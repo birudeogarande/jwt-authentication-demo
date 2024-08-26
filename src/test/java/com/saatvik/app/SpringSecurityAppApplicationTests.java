23bd555abff243e5b423e5d15ebb4e9781084dad
Birudeo Garande <birudeogarande@gmail.com>
2024-08-26 21:05:49 +0530

    added sayHello rest api unit testcase

--- src/test/java/com/saatvik/app/SpringSecurityAppApplicationTests.java
+++ src/test/java/com/saatvik/app/SpringSecurityAppApplicationTests.java
@@ -1,7 +1,6 @@
 package com.saatvik.app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -15,9 +14,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class SpringSecurityAppApplicationTests {
 
-	@Autowired
-	private MockMvc mvc;
-	@Test
+
+	private final MockMvc mvc;
+
+    SpringSecurityAppApplicationTests(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
 	void contextLoads() {
 	}
 
